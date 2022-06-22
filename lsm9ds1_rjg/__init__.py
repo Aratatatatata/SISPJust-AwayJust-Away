b126424e4cab1ed680f7231b7055aeb2b0ad9964
1020211NaotoYamaguhci <105772696+1020211NaotoYamaguhci@users.noreply.github.com>
2022-06-22 16:31:04 +0900

    Add files via upload

---
+++ lsm9ds1_rjg/__init__.py
@@ -0,0 +1,7 @@
+from .ag_status import AGStatus
+from .driver import Driver
+from .i2c_transport import I2CTransport
+from .spi_transport import SPITransport
+from .magnetometer_status import MagnetometerStatus
+
+name = "lsm9ds1_rjg"
