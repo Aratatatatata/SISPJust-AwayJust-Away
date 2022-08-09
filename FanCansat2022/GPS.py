1fad7f8f9cf8535a2ae2ded0bfc5ba4babd9aed3
1020211NaotoYamaguhci <105772696+1020211NaotoYamaguhci@users.noreply.github.com>
2022-08-09 16:46:00 +0900

    Update GPS.py

--- FanCansat2022/GPS.py
+++ FanCansat2022/GPS.py
@@ -1,6 +1,27 @@
-from micropyGPS import MicropyGPS
 import serial
-my_gps = MicropyGPS()
-my_sentence = serial.Serial().readline.decode('UTF-8')
-for x in my_sentence:
-     my_gps.update(x)
+from . import micropyGPS
+import threading
+import time
+
+class GPS(threading.Thread): 
+  def __init__(self): 
+      super(myThread, self).__init__()
+      self.gps = micropyGPS.MicropyGPS(9, 'dd') # MicroGPSオブジェクトを生成する。
+                                     # 引数はタイムゾーンの時差と出力フォーマット
+
+  def run(self):#使うときはrun()じゃなくてstart()
+      self.s = serial.Serial('/dev/serial0', 9600, timeout=10)
+      self.s.readline() # 最初の1行は中途半端なデーターが読めることがあるので、捨てる
+      while True:
+          sentence = s.readline().decode('utf-8') # GPSデーターを読み、文字列に変換する
+          if sentence[0] != '$': # 先頭が'$'でなければ捨てる
+              continue
+          for x in sentence: # 読んだ文字列を解析してGPSオブジェクトにデーターを追加、更新する
+              gps.update(x)
+               
+if __name__ == '__main__':
+     Gps = GPS.GPS()
+     Gps.start()
+     x = Gps.latitude[0]
+     y = Gps.longitude[0]
+     print('緯度経度: %2.8f, %2.8f' % (gps.latitude[0], gps.longitude[0]))
