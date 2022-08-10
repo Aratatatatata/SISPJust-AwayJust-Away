3f35840e0c62cbe62bdfdc3f6e575c38a704d177
1020211NaotoYamaguhci <105772696+1020211NaotoYamaguhci@users.noreply.github.com>
2022-08-10 15:52:19 +0900

    Update main.py

--- FanCansat2022/main.py
+++ FanCansat2022/main.py
@@ -10,24 +10,24 @@ from . import GPS
 from . import sarvo
 from . import Controller
 from . import 
-
+#csvwiterがインポートされてない
 def main():
     
+#インスタンスの作成
+
     con = Controller.Controller()
-    my_csv = MakeCSV()
-    
-    time.sleep(0.2)
-    lsm = simple.LSM9DS1()
-    gps = GPS.GPS(9,'dd')
+    my_csv = MakeCSV()#まちがってる
+    gps = GPS.GPS()
     gps.start()
     count = 0
-    while True:
-        gps.thread_acquire()
-        data = gps.get_data()
-        gps.thread_release()
-        acc_x, acc_y, acc_z = lsm.read_acc()
-        my_csv.write([data['date'][0],data['date'][1],data['date'][2],data['timestamp'][0],data['timestamp'][1],data['timestamp'][2],data['latitude'][0],data['latitude'][1],data['longitude'][0],data['longitude'][1],acc_x,acc_y,acc_z,0,0])
-        if(-5000 < acc_z and acc_z < 5000):
+
+#落下検知
+　while True:
+          acc_x, acc_y, acc_z = lsm.read_acc() #加速度の取得
+#GPSのデータをcsvに出力する.gps.data, gps.timestamp, gps.latitude, gps.longitudeを使用(検証する)
+my_csv.write([data['date'][0],data['date'][1],data['date'][2],gps.timestamp[0],gps.timestamp[1],data['timestamp'][2],gps.latitude[0],gps.latitude'][1],data['longitude'][0],data['longitude'][1],acc_x,acc_y,acc_z,0,0])  
+     
+　 if(-5000 < acc_z and acc_z < 5000):＃全部の値が規定値以下になれば開始
             count += 1
         else:
             count = 0
@@ -36,42 +36,27 @@ def main():
             break
         time.sleep(0.05)
 
-    gps.thread_acquire()
-    data = gps.get_data()
-    gps.thread_release()
+#制御開始
 
-    gps_x = float(data['longitude'][0])
-    gps_y = float(data['latitude'][0])
+    gps_x = gps.longitude[0]
+    gps_y = gps.latitude[0]
 
     acc_x, acc_y, acc_z = lsm.read_acc()
     my_csv.write([data['date'][0],data['date'][1],data['date'][2],data['timestamp'][0],data['timestamp'][1],data['timestamp'][2],data['latitude'][0],data['latitude'][1],data['longitude'][0],data['longitude'][1],acc_x,acc_y,acc_z,0,0])
-
-    con.uturn(gps_x, gps_y, 0, 0)
-    time.sleep(1.5)
-
     old_x, old_y = gps_x, gps_y
-
-    gps.thread_acquire()
-    data = gps.get_data()
-    gps.thread_release()
-
-    #print('while')
     i = 0
-    while True:
-        gps.thread_acquire()
-        data = gps.get_data()
-        gps.thread_release()
 
-        gps_x = float(data['longitude'][0])
-        gps_y = float(data['latitude'][0])
+    
+while True:
+        gps_x = gps.longitude[0]
+        gps_y = gps.latitude[0]
         
-        dir_x, dir_y = con.direction(gps_x, gps_y)
-        LR, let = con.dis_gps(old_x, old_y, gps_x, gps_y, dir_x, dir_y)
-        my_csv.write([data['date'][0],
-            data['date'][1],data['date'][2],data['timestamp'][0],data['timestamp'][1],
-            data['timestamp'][2],data['latitude'][0],data['latitude'][1],data['longitude'][0],data['longitude'][1],
+        dir_x, dir_y = con.set_goal(gps_x, gps_y)
+        let = con.dis_gps(old_x, old_y, gps_x, gps_y, dir_x, dir_y)
+        my_csv.write([data['date'][0],data['date'][1],data['date'][2],data['timestamp'][0],data['timestamp'][1],data['timestamp'][2],data['latitude'][0],data['latitude'][1],data['longitude'][0],data['longitude'][1],
             acc_x,acc_y,acc_z,LR,let])
-        old_x, old_y = gps_x, gps_y
+        
+       old_x, old_y = gps_x, gps_y
         time.sleep(0.5)
         i += 1
         if(i > 60):
