09e7e5777fa2717844a605e5fd5666da312cdc54
1020211NaotoYamaguhci <105772696+1020211NaotoYamaguhci@users.noreply.github.com>
2022-08-10 15:09:26 +0900

    Update Controller.py

--- FanCansat2022/Controller.py
+++ FanCansat2022/Controller.py
@@ -34,11 +34,11 @@ class Controller():
             
 
     #ひとつ前のgpsと今のgps、今のgpsとgoal、それぞれの傾きから右左を出す
-    def dis_gps(self, old_x, old_y, gps_x, gps_y, dir_x, dir_y):
-        dis_now_x = abs(gps_x - old_x)
-        dis_now_y = abs(gps_y - old_y)
-        dis_goal_x = abs(dir_x - gps_x)
-        dis_goal_y = abs(dir_y - gps_y)
+    def dis_gps(self, old_x, old_y, now_x, now_y, dir_x, dir_y):
+        dis_now_x = now_x - old_x 
+        dis_now_y = now_y -old_y
+        dis_goal_x = dir_x - now_x
+        dis_goal_y = dir_y - now_y
         asin_now = math.degrees(math.atan2(dis_now_y, dis_now_x))#傾きを角度にする
         asin_goal = math.degrees(math.atan2(dis_goal_y, dis_goal_x))#傾きを角度にする
         let = asin_goal - asin_now    #ゴールが左右どちらにあるかを出す。この値が正の値であればgoalは左にある。
