3f302d91c4b028c4549c15d1ea9b4052608eeed1
1020211NaotoYamaguhci <105772696+1020211NaotoYamaguhci@users.noreply.github.com>
2022-08-15 13:05:22 +0900

    Update main.py

--- FanCansat2022/main.py
+++ FanCansat2022/main.py
@@ -9,7 +9,7 @@ import sys
 from . import GPS
 from . import sarvo
 from . import Controller
-from . import 
+from . import csvWriter
 #csvwiterがインポートされてない
 def main():
     
@@ -25,7 +25,7 @@ def main():
 　while True:
           acc_x, acc_y, acc_z = lsm.read_acc() #加速度の取得
 #GPSのデータをcsvに出力する.gps.data, gps.timestamp, gps.latitude, gps.longitudeを使用(検証する)
-my_csv.write([data['date'][0],data['date'][1],data['date'][2],gps.timestamp[0],gps.timestamp[1],data['timestamp'][2],gps.latitude[0],gps.latitude'][1],data['longitude'][0],data['longitude'][1],acc_x,acc_y,acc_z,0,0])  
+my_csv.write([gps.timestamp[0],gps.timestamp[1],gps.timestamp[2],gps.latitude[0],gps.longitude[0],acc_x,acc_y,acc_z,0,0])  
      
 　 if(-5000 < acc_z and acc_z < 5000):＃全部の値が規定値以下になれば開始
             count += 1
@@ -42,7 +42,7 @@ my_csv.write([data['date'][0],data['date'][1],data['date'][2],gps.timestamp[0],g
     gps_y = gps.latitude[0]
 
     acc_x, acc_y, acc_z = lsm.read_acc()
-    my_csv.write([data['date'][0],data['date'][1],data['date'][2],data['timestamp'][0],data['timestamp'][1],data['timestamp'][2],data['latitude'][0],data['latitude'][1],data['longitude'][0],data['longitude'][1],acc_x,acc_y,acc_z,0,0])
+    my_csv.write([gps.timestamp[0],gps.timestamp[1],gps.timestamp[2],gps.latitude[0],gps.longitude[0],acc_x,acc_y,acc_z,0,0])
     old_x, old_y = gps_x, gps_y
     i = 0
 
@@ -53,7 +53,7 @@ while True:
         
         dir_x, dir_y = con.set_goal(gps_x, gps_y)
         let = con.dis_gps(old_x, old_y, gps_x, gps_y, dir_x, dir_y)
-        my_csv.write([data['date'][0],data['date'][1],data['date'][2],data['timestamp'][0],data['timestamp'][1],data['timestamp'][2],data['latitude'][0],data['latitude'][1],data['longitude'][0],data['longitude'][1],let])
+        my_csv.write([gps.timestamp[0],gps.timestamp[1],gps.timestamp[2],gps.latitude[0],gps.longitude[0],let])
          #サーボモータを動かす
 	     if let > 0:
              l_servo.write(180)
