de0cda3a8508eabf260fb466eb55ffb2079ea06d
1020211NaotoYamaguhci <105772696+1020211NaotoYamaguhci@users.noreply.github.com>
2022-08-17 15:28:04 +0900

    Update main.py

--- FanCansat2022/main.py
+++ FanCansat2022/main.py
@@ -19,6 +19,8 @@ def main():
     my_csv = csvWriter.csvWriter()
     gps = GPS.GPS()
     bmx = bmx055.bmx055()
+    l_servo=servo(12)
+    r_servo=servo(18)
     gps.start()
     count = 0
 
@@ -26,7 +28,7 @@ def main():
 　while True:
     acc = bmx.acc_value() #加速度の取得
     #GPSのデータをcsvに出力する.gps.data, gps.timestamp, gps.latitude, gps.longitudeを使用(検証する)
-    my_csv.write([gps.timestamp[0],gps.timestamp[1],gps.timestamp[2],gps.latitude[0],gps.longitude[0],acc_x,acc_y,acc_z,0,0])  
+    my_csv.write([gps.gps.timestamp[0],gps.gps.timestamp[1],gps.gps.timestamp[2],gps.gps.latitude[0],gps.gps.longitude[0],acc[0],acc[1],acc[2],0,0])  
      
 　  if(-50 < acc[0] and acc[0] < 50    and    -50 < acc[1] and acc[1] < 50    and    -50 < acc[2] and acc[2] < 50):#全部の値が規定値以下になれば開始
             count += 1
@@ -39,22 +41,22 @@ def main():
 
 #制御開始
 
-  gps_x = gps.longitude[0]
-  gps_y = gps.latitude[0]
+  gps_x = gps.gps.longitude[0]
+  gps_y = gps.gps.latitude[0]
 
   acc = bmx.acc_value()
-  my_csv.write([gps.timestamp[0],gps.timestamp[1],gps.timestamp[2],gps.latitude[0],gps.longitude[0],acc[0],acc[1],acc[2],0,0])
+  my_csv.write([gps.gps.timestamp[0],gps.gps.timestamp[1],gps.gps.timestamp[2],gps.gps.latitude[0],gps.gps.longitude[0],acc[0],acc[1],acc[2],0,0])
   old_x, old_y = gps_x, gps_y
   i = 0
 
     
   while True:
-        gps_x = gps.longitude[0]
-        gps_y = gps.latitude[0]
+        gps_x = gps.gps.longitude[0]
+        gps_y = gps.gps.latitude[0]
         
         dir_x, dir_y = con.set_goal(gps_x, gps_y)
         let = con.dis_gps(old_x, old_y, gps_x, gps_y, dir_x, dir_y)
-        my_csv.write([gps.timestamp[0],gps.timestamp[1],gps.timestamp[2],gps.latitude[0],gps.longitude[0],let])
+        my_csv.write([gps.gps.timestamp[0],gps.gps.timestamp[1],gps.timestamp[2],gps.latitude[0],gps.longitude[0],let])
          #サーボモータを動かす
 	     if let > 0:
              l_servo.write(180)
